8c5f023771243a2cef2abed92248b984596e9230
Pravu chandan Rout <rout01828@gmail.com>
2024-12-25 21:16:48 +0530

    added firebase in project

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7K_FNbBgTl16AqiDShwMUv8u55mKDGSc",
+  authDomain: "chatbox-7f430.firebaseapp.com",
+  projectId: "chatbox-7f430",
+  storageBucket: "chatbox-7f430.firebasestorage.app",
+  messagingSenderId: "1067337581613",
+  appId: "1:1067337581613:web:62541de5129b53560624a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
